10ab1d10470750b0780cf521dcc0e5b506893ca0
samg9 <9gsameer@gmail.com>
2020-02-20 18:43:57 -0800

    Add palette size selecter

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './ImageLinkForm.css'
 
-const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
+const ImageLinkForm = ({ onPaletteSizeChange, onInputChange, onButtonSubmit }) => {
 
   return (
     <div>
-      <b className=' w-80 f1 white light-purple'>Couleur (beta)</b>
-      <h4 className='light-purple'>Displays 5 color palette for each post on a public instagram account</h4>
-      <b className=' w-80 f3 white light-purple'>
+      <b className=' w-80 f1 white '>Couleur (beta)</b>
+      <h4 className='light-purple'>Displays color palette for each post on a public instagram account</h4>
+      <b className=' w-80 f3 white '>
         Enter instagram username
       </b>
 
@@ -15,6 +15,15 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
       <div className='center'>
         <div className='form  center pa4 br3 shadow-5'>
           <input className=' f4 pa3 w-70 center' type='tex' onChange={onInputChange} />
+
+          <select className="form-control form-control-lg" defaultValue="6" onChange={onPaletteSizeChange}>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+            <option>6</option>
+            <option>7</option>
+          </select>
           <button className=' w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
             onClick={onButtonSubmit}
           >
