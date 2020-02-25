b124e450c5d45dfa6c6628174294f1bb0c975c9e
samg9 <9gsameer@gmail.com>
2020-02-24 23:16:09 -0800

    Add copy to clipboard onClicks

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -23,6 +23,8 @@ const ImageLinkForm = ({ onPaletteSizeChange, onInputChange, onButtonSubmit }) =
             <option>5</option>
             <option>6</option>
             <option>7</option>
+            <option>8</option>
+            <option>9</option>
           </select>
           <button className=' w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
             onClick={onButtonSubmit}
